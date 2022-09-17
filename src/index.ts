186cd38ffeca80e86a6d712660fc182f7080ee67
Dmitrii Bormotov <squier7@gmail.com>
2022-09-17 22:35:27 +0500

    Update index.ts to call handler function only

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import 'dotenv/config'
-import {Market} from 'helpers/codes'
-import {getDatabaseConnection, saveTickers} from 'helpers/database'
-import {getBinanceTickers, getBitfinexTickers, getPoloniexTickers} from 'scrappers'
+import handler from './handler'
 
-// prettier-ignore
-;(async () => {
-  const db = await getDatabaseConnection()
-  await Promise.all([
-    saveTickers(db, Market.Bitfinex, await getBitfinexTickers()),
-    saveTickers(db, Market.Binance, await getBinanceTickers()),
-    saveTickers(db, Market.Poloniex, await getPoloniexTickers()),
-  ])
-})()
+handler()
