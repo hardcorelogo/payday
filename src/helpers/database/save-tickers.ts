85733ebafa136ff76830a1d2ec4725c1548f3c12
Dmitrii Bormotov <squier7@gmail.com>
2022-09-18 20:50:33 +0500

    Add decryption for collection parsing

--- src/helpers/database/save-tickers.ts
+++ src/helpers/database/save-tickers.ts
@@ -1,9 +1,12 @@
 import {Firestore} from 'firebase-admin/firestore'
 import {Market} from 'helpers/codes'
 import {Ticker} from 'scrappers'
+import {decrypt} from 'helpers/decrypt'
 
 async function saveTickers(db: Firestore, market: Market, data: Ticker) {
-  return db.collection(process.env.COLLECTION).doc(market).set(data)
+  const isEncrypted = process.env.ENCRYPTED != undefined
+  const collection = isEncrypted ? await decrypt(process.env.COLLECTION) : process.env.COLLECTION
+  return db.collection(collection).doc(market).set(data)
 }
 
 export {saveTickers}
